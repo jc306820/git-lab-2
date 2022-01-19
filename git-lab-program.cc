72b7f83029f96a8c1b746f95ba1f9390919c367b
Jacob Cox <jc306820@ohio.edu>
2022-01-19 18:05:06 -0500

    This is the Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jacob Cox
+ *        Date: 1-19-2022
+ * Description: Git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
